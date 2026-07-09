a7ac16f8925d16c06e9f609e23ee676463a8a2ef
MargusLiiva <levoliiva.ehitus@gmail.com>
2026-07-09 19:47:28 +0300

    Set up Express server with basic routes

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,20 @@
+const express = require("express");
 
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Media Command Center работает!");
+});
+
+app.post("/api/post", (req, res) => {
+  console.log(req.body);
+  res.send("Пост получен");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server started");
+});
